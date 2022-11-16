7a34afba094065055b392a158b836003b9a28146
ybouali <yassine.bouali.bo@gmail.com>
2022-11-16 18:43:41 +0100

    WORKING ON COPY CONSTRACTOR

--- vector/tests/main.cpp
+++ vector/tests/main.cpp
@@ -1,11 +1,23 @@
 
 #include <iostream>
 #include <vector>
+#include <typeinfo>
+
 
 #include "../vector.hpp"
 
 int main()
 {
-    std::vector< int, std::allocator<int> > v1;
-    ft::Vector< int, std::allocator<int> > v;
+    // TESTS FOR MY VECTOR
+    // std::vector<int> v;
+    // for (int i = 0; i < 3; i++)
+    //     v.push_back(i);
+    // for (int i = 0; i < 3; i++)
+    //     std::cout << "V [" << i << "] = " << v[i] << std::endl;
+    // ft::vector< int > v1 = v;
+    // for (int i = 0; i < 3; i++)
+    //     std::cout << "V [" << i << "] = " << v1[i] << std::endl;
+    // std::cout << v1.size() << std::endl;
+    // std::cout << v1.capacity() << std::endl;
+    
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -2,14 +2,58 @@ namespace ft
 {
     template < class T, class Alloc = std::allocator<T> >
     class vector {
+        private:
+            T                       *arr;
+            std::size_t             size_v;
+            std::size_t             capacity_v;
+            Alloc                   alloc;
         public:
-            vector()
+            // explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type());
+            // template <class InputIterator> vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
+            explicit vector (const Alloc& alloc = Alloc())
             {
-
+                (void)alloc;
+                this->arr = NULL;
+                this->size_v = 0;
+                this->capacity_v = 0;
             }
-            ~vector()
+            vector (const vector& other)
             {
-                
+                *this = other;
+            }
+            vector& operator= (const vector& other)
+            {
+                if (this->size_v > 0)
+                {
+                    for (size_t i = 0; i < this->size_v; i++)
+                    this->alloc.destroy(this->arr + i);
+                    if (this->capacity_v > 0)
+                        this->alloc.deallocate(this->arr, this->capacity_v);
+                    this->arr = NULL;
+                }
+                this->arr = this->alloc.allocate(other->capacity_v);
+                this->capacity_v = other->capacity_v;
+                this->size_v = other->size_v;
+                for (size_t i = 0; i < other->size_v; i++)
+                    this->arr[i] = other->arr[i];
+            }
+            ~vector (void)
+            {
+                for (size_t i = 0; i < this->size_v; i++)
+                    this->alloc.destroy(this->arr + i);
+                if (this->capacity_v > 0)
+                    this->alloc.deallocate(this->arr, this->capacity_v);
+                this->arr = NULL;
+            }
+            // reference operator[] (size_t n);
+            // const_reference operator[] (size_t n) const;
+            std::size_t size(void)
+            {
+                return this->size_v;
+            }
+            std::size_t capacity(void)
+            {
+                return this->capacity_v;
             }
     };
 }
