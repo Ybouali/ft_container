16952e7c5c4f398cea7d0e9453955f5446a12a4c
ybouali <yassine.bouali.bo@gmail.com>
2022-11-16 10:50:14 +0100

    START FT_CONTAINER PROJECT

---
+++ vector/tests/main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <vector>
+
+#include "../vector.hpp"
+
+int main()
+{
+    std::vector< int, std::allocator<int> > v1;
+    ft::Vector< int, std::allocator<int> > v;
+}
---
+++ vector/vector.hpp
@@ -0,0 +1,15 @@
+namespace ft
+{
+    template < class T, class Alloc = std::allocator<T> >
+    class vector {
+        public:
+            vector()
+            {
+
+            }
+            ~vector()
+            {
+                
+            }
+    };
+}
