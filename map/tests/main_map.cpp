5b5010a0cc0f6dff5bc183000b7a50e97505474e
ybouali <yassine.bouali.bo@gmail.com>
2022-12-24 05:45:30 +0100

    ERROR ON LEFT ROTATION

--- map/tests/main_map.cpp
+++ map/tests/main_map.cpp
@@ -6,18 +6,23 @@
 int     main()
 {
     
-    // , std::greater<int>
+    // std::greater<int>
     ft::map<int, std::string >   map;
+
+    // map.insert(ft::make_pair(10, "TEST :)"));
+    // map.insert(ft::make_pair(18, "TEST :)"));
+    // map.insert(ft::make_pair(8, "TEST :)"));
+    // map.insert(ft::make_pair(9, "TEST :)"));
+    // map.insert(ft::make_pair(2, "TEST :)"));
+    // map.insert(ft::make_pair(20, "TEST :)"));
+    // map.insert(ft::make_pair(17, "TEST :)"));
+    
 
     map.insert(ft::make_pair(10, "TEST :)"));
     map.insert(ft::make_pair(18, "TEST :)"));
-    map.insert(ft::make_pair(8, "TEST :)"));
-    map.insert(ft::make_pair(9, "TEST :)"));
-    map.insert(ft::make_pair(2, "TEST :)"));
+    // map.insert(ft::make_pair(19, "TEST :)"));
     map.insert(ft::make_pair(20, "TEST :)"));
-    map.insert(ft::make_pair(17, "TEST :)"));
+    // map.insert(ft::make_pair(7, "TEST :)"));
+    // map.insert(ft::make_pair(15, "TEST :)"));
     
-    // std::map<int, std::string> std_map;
-
-    // std_map.insert(std::make_pair(10, "TEST"));
 }
